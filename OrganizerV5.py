f9aff2300701919cbba144bcf2e465786ea96b4f
AiTrainr <126282124+AiTrainr@users.noreply.github.com>
2023-02-24 14:00:54 -0800

    Update OrganizerV5.py

--- OrganizerV5.py
+++ OrganizerV5.py
@@ -33,7 +33,7 @@ class InventoryApp:
         # Create file menu
         file_menu = tk.Menu(menubar, tearoff=False)
         menubar.add_cascade(label='File', menu=file_menu)
-        file_menu.add_command(label='Add Item', command=self.show_add_item_popup)
+        file_menu.add_command(label='Add Item', command=self.inventory_frame.show_add_item_popup)
         file_menu.add_command(label='Edit Item', command=self.show_edit_item_popup)
         file_menu.add_separator()
         file_menu.add_command(label='Exit', command=self.master.quit)
